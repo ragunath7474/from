66d0705caeb5f420c6fad4cd2433895fa39e6fea
ragunath7474 <you@example.com>
2022-09-16 13:59:30 +0530

    first commit

---
+++ asset/js/funtion.js
@@ -0,0 +1,17 @@
+function ragu(){
+    console.log(' helloragunath ');
+}
+ragu();
+
+
+
+// let ragu2=function(){
+//     console.log(' hellogokul ')
+// }
+// ragu();
+
+
+// let ragu3=()=>{
+//     console.log(' helloajay ')
+// }
+// ragu();
